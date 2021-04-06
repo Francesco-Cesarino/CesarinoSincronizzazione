a48708d084d0348028730616f21d9c3c4631f9a0
Francesco-Cesarino <francescocesarino.fc@gmail.com>
2021-04-06 19:56:49 +0200

    CesarinoSincronizzazione

---
+++ src/main/java/CesarinoSincronizzazione/ThreadDecrementa.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CesarinoSincronizzazione;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Francesco
+ */
+public class ThreadDecrementa implements Runnable
+{
+    Counter contatore = new Counter();
+
+    @Override
+    public void run() {
+        
+        for(int i = 0; i < 10000; i++){
+        
+            contatore.incDec(false);
+            
+            try {
+                Thread.sleep(0);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ThreadDecrementa.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
